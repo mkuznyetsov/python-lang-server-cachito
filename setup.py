c83feb121b2d5a1929393fe4f8af262f3a887955
Mykhailo Kuznietsov <mkuznets@redhat.com>
2022-08-22 14:51:26 +0300

    fixup! create requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Python language server',
+      version='1.0',
+      description='Python Language Server',
+      author='Mykhailo Kuznietsov',
+      author_email='mkuznets@redhat.com',
+     )
